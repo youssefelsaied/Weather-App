fdeaaa403ff57033e2e1337223d68c3e1761f7d9
Adefioye <abdulhakeemadefioye@gmail.com>
2024-04-12 00:20:00 -0500

    Add test to validate input text

--- test/src/features/weather/data/city_search_box_test.dart
+++ test/src/features/weather/data/city_search_box_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:open_weather_example_flutter/src/features/weather/presentation/city_search_box.dart';
+import 'package:open_weather_example_flutter/src/features/weather/presentation/weather_page.dart';
 
 class MockCityProvider extends Mock implements Ref {}
 
@@ -34,28 +35,28 @@ void main() {
       expect(find.text('search'), findsOneWidget);
     });
 
-    // testWidgets('Input text into search box', (WidgetTester tester) async {
-    //   when(mockCityProvider.read).thenReturn((ref) => '');
+   testWidgets('Input text into search box', (WidgetTester tester) async {
+    final mockCityProvider = Provider((ref) => '');  // Mock function that provides an empty string
 
-    //   await tester.pumpWidget(
-    //     MaterialApp(
-    //       home: Scaffold(
-    //         body: ProviderScope(
-    //           overrides: [
-    //             Provider((ref) => mockCityProvider), // Override cityProvider
-    //           ],
-    //           child: WeatherPage(city: ''),
-    //         ),
-    //       ),
-    //     ),
-    //   );
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: ProviderScope(
+            overrides: [
+              Provider((ref) => mockCityProvider), // Override cityProvider
+            ],
+            child: const WeatherPage(city: ''),
+          ),
+        ),
+      ),
+    );
 
-    //   final inputField = find.byType(TextField);
-    //   expect(inputField, findsOneWidget);
+    final inputField = find.byType(TextField);
+    expect(inputField, findsOneWidget);
 
-    //   await tester.enterText(inputField, 'New York');
-    //   expect(find.text('New York'), findsOneWidget);
-    // });
+    await tester.enterText(inputField, 'New York');
+    expect(find.text('New York'), findsOneWidget);
+});
 
   });
 }
